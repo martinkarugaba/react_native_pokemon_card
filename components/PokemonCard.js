1bc45bb35f128e48ba166fcd21e478d49859bf7d
Martin Karugaba <martinkarugaba21@gmail.com>
2024-03-01 23:32:49 +0300

    Add initial styles to pokemon card

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -10,11 +10,15 @@ export default function PokemonCard({
 }) {
   return (
     <View style={styles.card}>
-      <View>
-        <Text>{name}</Text>
-        <Text>{occupation}</Text>
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.occupation}>{occupation}</Text>
       </View>
-      {/* <Image source={image} accessibilityLabel={`${name}`} /> */}
+      <Image
+        source={image}
+        style={styles.image}
+        accessibilityLabel={`${name}`}
+      />
       <View>
         <Text>Years of Experience: {yearsOfExperience}</Text>
       </View>
@@ -47,5 +51,26 @@ const styles = StyleSheet.create({
         elevation: 4,
       },
     }),
+  },
+  nameContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 32,
+  },
+  name: {
+    fontSize: 30,
+    fontWeight: '700',
+    color: '#212121',
+  },
+  occupation: {
+    fontSize: 22,
+  },
+  image: {
+    width: '100%',
+    height: 350,
+    marginBottom: 16,
+    borderWidth: 2,
+    borderColor: 'blue',
+    resizeMode: 'contain',
   },
 });
